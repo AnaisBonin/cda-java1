79f000c7521991ab8c6377ea4165f9ab1ac79b6a
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-15 10:28:25 +0100

    Java - For and While loop

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args){
+        System.out.println("Boucle WHILE");
+        int i = 0;
+        while (i < 101) {
+           System.out.println("i = " + i);
+           i++;
+        }
+    }
+}
