25b9e24ea6cd2c61f4ff4d38eff9e9d6e82b978a
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-15 16:36:28 +0100

    Java - Exo3: 3.2 Write approximation_fibo method

--- src/exo3.java
+++ src/exo3.java
@@ -4,6 +4,9 @@ public class exo3 {
     public static void main(String[] args) {
         System.out.println("EXO 3");
         fibo(1);
+        int[] result = approximation_fibo((float)0.01);
+        System.out.println("approx");
+        System.out.println(Arrays.toString(result));
     }
 
     private static int[] fibo(int n) {
@@ -17,13 +20,31 @@ public class exo3 {
             int i = 2;
             while (i < n) {
                 fibonacciSeq[i] = fibonacciSeq[i - 1] + fibonacciSeq[i - 2];
-
                 i++;
             }
-
-            System.out.println(n + " numbers of Fibonacci");
-            System.out.println(Arrays.toString(fibonacciSeq));
             return fibonacciSeq;
         }
+    }
+
+    private static int[] approximation_fibo(float epsilon) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        int[] result = new int[2];
+        result[0] = 0;
+
+        int j = 4;
+
+        while (result[0] == 0) {
+            int a = fibo(j)[j - 2];
+            int b = fibo(j)[j - 1];
+            double ratioBA = (double) b / a;
+            double diff = Math.abs(ratioBA - phi);
+
+            if (diff < epsilon) {
+                result[0] = b;
+                result[1] = a;
+            }
+            j++;
+        }
+        return result;
     }
 }
