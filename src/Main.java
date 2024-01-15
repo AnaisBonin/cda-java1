67cca34f37d94149e51b2727ebcd07afb0b63c4f
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-15 11:15:18 +0100

    Java - combine all methods under one single class Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Boucle FOR");
 
         for (int i = 1; i <= 100; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+        }
+        enumerate_while(250);
+        enumerate_even(150);
+    }
+
+    private static void enumerate_while(int max){
+        System.out.println("Boucle WHILE");
+        int i = 0;
+        while (i <= max) {
+            System.out.println("i = " + i);
+            i++;
+        }
+    }
+
+    private static void enumerate_even(int max) {
+        System.out.println("Boucle FOR - print even numbers");
+
+        for (int i = 0; i <= max; i++) {
+            if (i%2 == 0) {
+                System.out.println("i = " + i);
+            }
         }
     }
 }
