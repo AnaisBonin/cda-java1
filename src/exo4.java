3ab252ce121be7cca4102ab66fc2547f3ef72635
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-16 11:29:39 +0100

    Java - Exo4 - change Array to ArrayList

--- src/exo4.java
+++ src/exo4.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class exo4 {
     public static void main(String[] args) {
-        String[] nameList = new String[]{"Jean", "Ahmed", "Lea", "Blanca"};
+        ArrayList<String> nameList = new ArrayList<String> (Arrays.asList("Jean", "Ahmed", "Lea", "Blanca"));
         describe(nameList);
 
     }
 
-    private static void describe(String[] ArrayList) {
+    private static void describe(ArrayList<String> ArrayList) {
         for (String name : ArrayList) {
             System.out.println(name + " : " + name.length());
         }
